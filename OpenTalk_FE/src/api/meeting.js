0e596a04a4ce813fb4a9e5bc5749a6aa76a9a498
minhsimple <88241158+minhsimple@users.noreply.github.com>
2025-07-12 22:04:43 +0700

    feat: update meeting.js

--- OpenTalk_FE/src/api/meeting.js
+++ OpenTalk_FE/src/api/meeting.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8080/api/opentalk-topic';
+const API_BASE = 'http://localhost:8080/api/opentalk-meeting';
 
 export const getMeetings = () =>
   axios.get(API_BASE).then(res => res.data);
