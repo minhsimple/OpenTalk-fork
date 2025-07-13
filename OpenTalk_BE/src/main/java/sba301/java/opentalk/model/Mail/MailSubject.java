7c4d6e0461dbb33fc1c9110d45efb61cd12d0e60
Nguyen Anh Vu <vunguyen.nav2004@gmail.com>
2025-07-13 13:01:41 +0700

    [OpenTalk_FE] update: separate be form project

--- OpenTalk_BE/src/main/java/sba301/java/opentalk/model/Mail/MailSubject.java
+++
@@ -1,5 +0,0 @@
-package sba301.java.opentalk.model.Mail;
-
-public interface MailSubject {
-    public String generateSubjectOfMail();
-}
--- OpenTalk_BE/src/main/java/sba301/java/opentalk/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package sba301.java.opentalk.repository;
-
-import sba301.java.opentalk.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
